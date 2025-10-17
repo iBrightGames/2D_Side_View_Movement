989fa6644861b4610655498f4333f3bc8893e35b
thehouseofcolors <rengin.celik97@gmail.com>
2025-10-17 14:04:01 +0300

    Demo scene added

--- 2D_Side_View/Assets/_ObjectMovementSystem/Scripts/Core/Enums.cs
+++ 2D_Side_View/Assets/_ObjectMovementSystem/Scripts/Core/Enums.cs
@@ -1,5 +1,5 @@
 
 
-public enum DOType { Move, MoveX, MoveY, Jump, Rotate, Path, LocalPath }
+// public enum DOType { Move, MoveX, MoveY, Jump, Rotate, Path, LocalPath }
 
-public enum LoopTypeCustom { None, Loop, PingPong }
+// public enum LoopTypeCustom { None, Loop, PingPong }
